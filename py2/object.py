ab3b94e0ea7432eb29576e357239a140e097b489
Mohdsohail7 <sohailmohd437@gmail.com>
2025-01-11 22:05:32 +0530

    feat: i added more api's working with object

--- py2/object.py
+++ py2/object.py
@@ -11,6 +11,16 @@ book = {
     'isAvailable': True,
     'stock': 5,
 }
+
+githubPublicData = {
+	"username": 'ankit123',
+	"fullName": 'Ankit Kumar',
+	"email": 'ankit@gmail.com',
+	"repositories": 24,
+	"gists": 12,
+	"joinedOn": 'Sep 2018',
+}
+
 # Create an endpoint that returns the details of a book stored on the server.
 def get_all_book(book):
     return book
@@ -72,6 +82,80 @@ def checkStockAndOrder(book):
 def stockAndBook():
     result = checkStockAndOrder(book)
     return jsonify(result)
+
+
+# i write some new apis for github profile
+# Define the function getProfileUrl to return the GitHub profile URL of the user.
+# Declare a GET endpoint /github-profile to use the getProfileUrl function.
+def getProfileUrl():
+    return f"{"https://github.com/"}{githubPublicData["username"]}"
+
+@app.route("/github-profile", methods = ["GET"])
+def github_profile():
+    result = getProfileUrl()
+    return jsonify({ "profileUrl": result})
+
+# Define the function getPublicEmail to return the GitHub email of the user.
+# Declare a GET endpoint /github-public-email to use the getPublicEmail
+def getPublicEmail():
+    return githubPublicData["email"]
+
+@app.route("/github-public-email", methods = ["GET"])
+def github_public_email():
+    result = getPublicEmail()
+    return jsonify({ "publicEmail": result })
+
+
+# Define the function getReposCount to return the number of repositories the user has.
+# Declare a GET endpoint /github-repos-count to use the getReposCount function.
+def getReposCount():
+    return githubPublicData["repositories"]
+
+@app.route("/github-repos-count", methods = ["GET"])
+def github_repos_count():
+    result = getReposCount()
+    return jsonify({ "reposCount": result })
+
+
+# Define the function getGistsCount to return the number of gists the user has.
+# Declare a GET endpoint /github-gists-count to use the getGistsCount function.
+def getGistsCount():
+    return githubPublicData["gists"]
+    
+@app.route("/github-gists-count", methods = ["GET"])
+def github_gists_count():
+    result = getGistsCount()
+    return jsonify({ "gistsCount": result })
+
+
+# Get User Bio
+# Define the function getUserBio to return the user's bio.
+# Declare a GET endpoint /github-user-bio to use the getUserBio function.
+def getUserBio():
+    return { "fullName": githubPublicData["fullName"], "joinedOn": githubPublicData["joinedOn"], "email": githubPublicData["email"]}
+
+@app.route("/github-user-bio", methods = ["GET"])
+def github_user_bio():
+    result = getUserBio()
+    return jsonify(result)
+
+
+# Repository URL
+# Define the function getRepoUrl to return the URL of a specific repository.
+# Declare a GET endpoint /github-repo-url to use the getRepoUrl function.
+def getRepoUrl(repoName):
+    baseUrl = "https://github.com/"
+    username = githubPublicData["username"]
+    return f"{baseUrl}{username}/{repoName}"
+    
+@app.route("/github-repo-url", methods = ["GET"])
+def github_repo_url():
+    repoName = request.args.get("repoName", 0)
+    result = getRepoUrl(repoName)
+    return jsonify({ "repoUrl": result })
+
+
+
 
 
 
